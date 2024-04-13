6543c9c75a14f7968897b54d73657c311b9d5ae6
suno <huberghajackson@gmail.com>
2024-04-13 08:06:27 +0100

    Small shader changes

--- pluginDev/Plugins/MakeBox/Intermediate/Build/Win64/x64/UnrealEditor/Development/MakeBox/Definitions.MakeBox.h
+++ pluginDev/Plugins/MakeBox/Intermediate/Build/Win64/x64/UnrealEditor/Development/MakeBox/Definitions.MakeBox.h
@@ -1,4 +1,4 @@
-#include "C:/Users/sunohonmy/Documents/Unreal Projects/pluginDev/Intermediate/Build/Win64/x64/pluginDevEditor/Development/UnrealEd/SharedDefinitions.UnrealEd.Cpp20.h"
+#include "C:/Users/sunohonmy/Desktop/Shaders_in_UE5/pluginDev/Intermediate/Build/Win64/x64/pluginDevEditor/Development/UnrealEd/SharedDefinitions.UnrealEd.Cpp20.h"
 #undef UE_IS_ENGINE_MODULE
 #undef DEPRECATED_FORGAME
 #define DEPRECATED_FORGAME DEPRECATED
